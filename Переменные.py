a32c9c187b8db4edd1731c0dee0c6d14ae03edb0
zlyuka <zhenya0699@gmail.com>
2024-08-27 20:55:48 +0300

    Add files via upload

---
+++ Переменные.py
@@ -0,0 +1,5 @@
+number_of_completed_homework_assignments = 12
+number_of_hours_spent = 1.5
+course_name = 'Python'
+time_for_one_task = (number_of_hours_spent / number_of_completed_homework_assignments)
+print(f'Курс:{course_name}, всего задач:, {number_of_completed_homework_assignments}, затрачено часов:{number_of_hours_spent}, среднее время выполнения {time_for_one_task}, часа.')
