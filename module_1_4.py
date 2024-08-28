a53bef0b6c528a52a7543950c71fc9f559e033e9
zlyuka <zhenya0699@gmail.com>
2024-08-28 23:18:15 +0300

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,7 @@
+my_string = str(input('Ввудите произвольный текст, пожалуйста: '))
+print(len(my_string))
+print(my_string .upper())
+print(my_string .lower())
+print(my_string .replace(' ', ''))
+print(my_string[0])
+print(my_string[-1])
